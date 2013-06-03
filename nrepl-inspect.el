9ad881a970a5cb6cc7486d732b6abb1236a849d8
Ian Eslick <eslick@alum.mit.edu>
2013-06-02 19:47:19 -0700

    Issue #1, copyright header

--- nrepl-inspect.el
+++ nrepl-inspect.el
@@ -1,3 +1,28 @@
+;;; nrepl.el --- Client for Clojure nREPL
+
+;; Copyright © 2013 Vital Reactor, LLC
+;;
+;; Author: Ian Eslick <ian@vitalreactor.com>
+;; URL: http://www.github.com/vitalreactor/nrepl-inspect
+;; Version: 0.1.0
+;; Keywords: languages, clojure, nrepl
+;; Package-Requires: ((clojure-mode "2.0.0"))
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; This file is not part of GNU Emacs.
+
 (require 'cl)
 (require 'nrepl)
 
@@ -252,3 +277,5 @@ that value.
            (slime-inspector-operate-on-point))
           (t
            (error "No clickable part here")))))
+
+(provide 'nrepl-inspect)
